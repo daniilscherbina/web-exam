85d89562f33574ecd6c45da4ad48d18f8972ee05
ausf-software <77770487+ausf-software@users.noreply.github.com>
2023-12-02 11:49:21 +0300

    basis

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import AboutUsBlock from './components/AboutUsBlock.vue'
+import HeaderBlock from './components/HeaderBlock.vue'
+import FooterBlock from './components/FooterBlock.vue'
+import ClientsBlock from './components/ClientsBlock.vue'
+import CostBlock from './components/CostBlock.vue'
+import FAQBlock from './components/FAQBlock.vue'
+import FormBlock from './components/FormBlock.vue'
+import LastCasesBlock from './components/LastCasesBlock.vue'
+import ReviewsBlock from './components/ReviewsBlock.vue'
+import SupportBlock from './components/SupportBlock.vue'
+import TeamBlock from './components/TeamBlock.vue'
+import TimeSpeedBlock from './components/TimeSpeedBlock.vue'
+</script>
+
+<template>
+  <HeaderBlock />
+
+  <main>
+  </main>
+
+  <FooterBlock />
+</template>
+
+<style scoped>
+
+</style>
