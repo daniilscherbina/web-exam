c360322edf030656a808d37caa4def5a2fb8e4e1
ausf-software <77770487+ausf-software@users.noreply.github.com>
2023-12-16 13:56:16 +0300

    files...

---
+++ src/components/header_block/MobileMenu.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="mobile-menu-line">
+      <img class="logo" src="../../assets/img/icons/drupal-coder.svg">
+      <div class="menu-button"></div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+  .mobile-menu-line {
+    height: 50px;
+    width: 100%;
+    background-color: #181818;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 1000;
+  }
+
+  .menu-button,
+  .menu-button::before,
+  .menu-button::after {
+    display: block;
+    background-color: #8e8e8e;
+    position: absolute;
+    right: 15px;
+    height: 4px;
+    width: 30px;
+    transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    border-radius: 2px;
+    margin-top: -15px;
+  }
+
+  .menu-button::before {
+    content: '';
+    margin-top: -8px;
+    margin-right: -15px;
+  }
+
+  .menu-button::after {
+    content: '';
+    margin-top: 8px;
+    margin-right: -15px;
+  }
+
+  @media (min-width: 1300px) {
+    .mobile-menu-line {
+      width: 0px;
+      height: 0px;
+      display: none;
+    }
+  }
+</style>
